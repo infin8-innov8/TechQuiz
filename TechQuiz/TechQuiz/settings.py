5f96d508a5664012aa65bda463bc6e0ce0990c2c
Pranav Vasankar <pranav.vasankar@gmail.com>
2026-01-29 10:08:56 +0530

    new changes doen

--- TechQuiz/TechQuiz/settings.py
+++ TechQuiz/TechQuiz/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'register',
+    'registration_n_login',
 ]
 
 MIDDLEWARE = [
